5d07a3e80a4621c8b5d5a17ec12a81f3a1e9b9cf
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-07-29 16:54:49 -0300

    Update exercicio18.py

--- exercicio18.py
+++ exercicio18.py
@@ -1,49 +1,62 @@
+import os
+sala = 0
+horario = 0
 reservas = [('Sala 1', '14h'), ('Sala 2', '10h'), ('Sala 3', '11h')]
+salas = ['Sala 1', 'Sala 2', 'Sala 3', 'Sala 4']
+horarios = ['10h', '11h', '12h', '13h', '14h']
+
+
 
 def verificar_disponibilidade(reservas, sala, horario):
-    for reserva in reservas:
-        salaReservada, horarioReservado = reserva
-
-        if salaReservada == sala and horarioReservado == horario:
-            return print('\nInfelizmente essa sala já está reservada nesse horário.')
-        
-    return print('\nSala reservada com sucesso!')
+        if (sala, horario) in reservas :
+            print(f'\nInfelizmente a {sala} já está reservada às {horario}.\n')
+       
+        else:
+            print(f'\n{sala} disponível às {horario}!\n')
     
+def adicionar_reserva(reservas, sala, horario):
+    if (sala, horario) in reservas :
+        print(f'\nInfelizmente a {sala} já está reservada para as {horario}.\n')
+    else:
+        reservas.append((sala, horario))
+        print(f'\nA {sala} foi reservada para as {horario}!\n')
+
+def receber_dados():
+    # global sala
+    # global horario
+    print('\n\nEscolha uma sala: ')
+    for i, sala in enumerate(salas):
+        print(f'{i + 1} -', sala)
+    sala = int(input('Insira: '))
+    while sala > 4 or sala < 1:
+        sala = int(input('Opção Inválida! Olhe novamente a disponibilidade.'))
+    sala = salas[sala - 1]
+    print('\n\nEscolha um horário para verificar: ')
+    for i, horario in enumerate(horarios):
+        print(f'{i + 1} -', horario)
+    horario = int(input('Insira: '))
+    while horario > 5 or horario < 1:
+        horario = int(input('Opção Inválida! Olhe novamente a disponibilidade. '))
+    horario = horarios[horario - 1]
+
+while True:
+    escolha = input('\nSelecione uma opção:\n1- Adicionar nova reserva\n2- Verificar reservas\n3- Listar reservas\n4- Sair\nInsira: ')
+    match escolha:
+        case '1':
+            os.system('cls')
+            receber_dados()
+            adicionar_reserva(reservas, sala, horario)
+        case '2':
+            receber_dados()
+            verificar_disponibilidade(reservas, sala, horario)
+        case '3':
+            print(reservas)
+        case '4':
+            print('Até a próxima!')
+            break
+        case _:
+            print('\nOpção inválida. Digite novamente.\n')
+            continue
 
 
-# Cria um menu 
-sala = input('Selecione uma dessas opções para realizar a sua reserva: \n1- Sala 1\n2- Sala 2\n3- Sala 3\n4- Sala 4\n5- Sala 5\nInsira: ')
-match sala:
-    case '1':
-        sala = 'Sala 1'
-    case '2':
-        sala = 'Sala 2'
-    case '3':
-        sala = 'Sala 3'
-    case '4':
-        sala = 'Sala 4'
-    case '5':
-        sala = 'Sala 5'
-    case _:
-        print('Erro! Sala inexistente. ')
-        
-
-
-
-horario = input('\nPara qual horário gostaria de reservar? Considere a disponibilidade de 1h para as salas: \n1- 10h\n2- 11h\n3- 12h\n4- 13h\n5- 14h\nInsira: ')
-match horario:
-    case '1':
-        horario = '10h'
-    case '2':
-        horario = '11h'
-    case '3':
-        horario = '12h'
-    case '4':
-        horario = '13h'
-    case '5':
-        horario = '14h'
-    case _:
-        print('Horário inexistente! ')
-
-
-verificar_disponibilidade(reservas = reservas, sala = sala, horario = horario)
+# Colocar lista organizada e perguntar se quer resolver
